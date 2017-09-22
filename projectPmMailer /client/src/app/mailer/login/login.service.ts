816b4d0dd6005f2f46bd5d65eccb671aacac57f2
pulkit.5.kakkar <pulkit.5.kakkar@niit-tech.com>
2017-09-22 12:32:24 +0530

    server deletted

--- projectPmMailer /client/src/app/mailer/login/login.service.ts
+++ projectPmMailer /client/src/app/mailer/login/login.service.ts
@@ -5,14 +5,22 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class LoginService {
 
-  
 
-  constructor(private http : Http) { } findUser(email,password){
+
+  constructor(private http: Http) {} findUser(email, password) {
     //console.log(credentials);
     const url = 'http://localhost:3000/api/signin/' + email + "/" + password;
     return this.http
-                            .get(url)
-                            .map(res => res.json());
-} 
+      .get(url)
+      .map(res => res.json());
+  }
 
+  //Method for google-auth
+  google() {
+    const url = 'http://localhost:3000/auth/google'
+    return this.http
+      .get(url)
+      .map(res => res);
+  }
+  //Method for google-auth end
 }
