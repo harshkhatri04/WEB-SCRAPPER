43701e262e9a781a8fd8d7a47b27130dbf6b4ae6
onlyshikhar <shikhar.akg@gmail.com>
2017-09-22 20:10:20 +0530

    comments

--- projectPmMailer /client/src/app/mailer/dashboard/dashboard.component.ts
+++ projectPmMailer /client/src/app/mailer/dashboard/dashboard.component.ts
@@ -6,7 +6,6 @@ import {NasdaqService} from './service/nasdaq.service';
  templateUrl: './dashboard.component.html',
  styleUrls: ['./dashboard.component.css','./animate.min.css','./light-bootstrap-dashboard.css','./demo.css','./pe-icon-7-stroke.css']
 })
-
 export class DashboardComponent implements OnInit { 
  constructor(private nas:NasdaqService) { }
  list:any=[]
@@ -16,10 +15,8 @@ val:any=['ETF','Bonds','Stocks','NASDAQ']
     
     a:any;    ngOnInit() {      
         this.nas.getnas().subscribe((data)=>{              
-        	this.list=data;console.log(this.list)   
-           })
+        	this.list=data;console.log(this.list)    })
 }
-
   search(name){
 this.a={
     term: name
