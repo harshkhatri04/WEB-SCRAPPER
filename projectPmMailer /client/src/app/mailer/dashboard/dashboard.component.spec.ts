c44017db0a9fb62167a34f437e3f83b7041c84b6
onlyshikhar <shikhar.akg@gmail.com>
2017-09-25 09:35:15 +0530

    test cases added

--- projectPmMailer /client/src/app/mailer/dashboard/dashboard.component.spec.ts
+++ projectPmMailer /client/src/app/mailer/dashboard/dashboard.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+/*import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DashboardComponent } from './dashboard.component';
 
@@ -22,4 +22,26 @@ describe('DashboardComponent', () => {
   it('should be created', () => {
     expect(component).toBeTruthy();
   });
+});
+*/
+
+
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import { DashboardComponent } from './dashboard.component';
+import{browser,element,by} from 'protractor';
+
+describe('LoginComponent', ()=>{
+
+
+beforeEach(()=>{
+    browser.get('http://localhost:4200');
+    
+})
+  it('should pass as user', function(){
+    
+    element(by.id('search')).click();
+    browser.pause();
+
+})
 });
