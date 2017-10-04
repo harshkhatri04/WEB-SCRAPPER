9fe79d386c0398191b902492a65b7f66dc93036c
pulkit.5.kakkar <pulkit.5.kakkar@niit-tech.com>
2017-10-04 09:51:08 +0530

    test cases

--- projectPmMailer /client/src/app/mailer/login/login.component.spec.ts
+++ projectPmMailer /client/src/app/mailer/login/login.component.spec.ts
@@ -1,31 +1,31 @@
  /*import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-        									import {RouterTestingModule} from '@angular/router/testing';
-        									import { LoginComponent } from './login.component';
-        									import{browser,element,by} from 'protractor';
+       									import {RouterModule} from '@angular/router/testing';
+       									import { LoginComponent } from './login.component';
+       									import{browser,element,by} from 'protractor';
 
-        									describe('LoginComponent', ()=>{
+       									describe('LoginComponent', ()=>{
 
 
-        									 beforeEach(()=>{
-        											browser.get('http://localhost:4200');
-        											
-        									 })
-        										it('should pass as user', function(){
-        											
-        										element(by.id('email')).sendKeys('user@gmail.com');
-        										 element(by.id('pwd')).sendKeys('password');
-        											element(by.id('loginbutton')).click();
-        											browser.pause();
+       									 beforeEach(()=>{
+       											browser.get('http://localhost:4200');
+       											
+       									 })
+       										it('should pass as user', function(){
+       											
+       										element(by.id('email')).sendKeys('user@gmail.com');
+       										 element(by.id('pwd')).sendKeys('password');
+       											element(by.id('loginbutton')).click();
+       											browser.pause();
 
-        									 })
+       									 })
 
-        									});*/
+       									});*/
 
  import { ComponentFixture, TestBed, async, inject } from '@angular/core/testing';
  import { By } from '@angular/platform-browser';
  import { DebugElement } from '@angular/core'
  import { LoginComponent } from './login.component'
- import { Router, RouterTestingModule, ActivatedRoute } from '@angular/router';
+ import { Router, RouterModule, ActivatedRoute } from '@angular/router';
  import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 
  import { LoginService } from './login.service'
@@ -34,7 +34,7 @@
  describe('LoginComponent', () => {
 
    let component: LoginComponent;
-   let fixture: ComponentFixture < LoginComponent > ;
+   let fixture: ComponentFixture< LoginComponent > ;
    let de: DebugElement;
    let el: HTMLInputElement;
 
@@ -54,7 +54,7 @@
        imports: [
          FormsModule,
          ReactiveFormsModule,
-         RouterTestingModule
+         RouterModule
        ],
        declarations: [
          LoginComponent
@@ -70,7 +70,7 @@
 
    beforeEach(() => {
      fixture = TestBed.createComponent(LoginComponent);
-     console.log(fixture)
+     console.log('hello')
      component = fixture.componentInstance;
    })
 
