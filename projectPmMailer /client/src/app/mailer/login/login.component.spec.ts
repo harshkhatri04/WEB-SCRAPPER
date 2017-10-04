d841bdbb1b5365e2e76f5d3267b4e7a0a6e0c006
pulkit.5.kakkar <pulkit.5.kakkar@niit-tech.com>
2017-10-04 15:16:11 +0530

    login component front-end test cases

--- projectPmMailer /client/src/app/mailer/login/login.component.spec.ts
+++ projectPmMailer /client/src/app/mailer/login/login.component.spec.ts
@@ -1,48 +1,45 @@
-/* import { ComponentFixture, TestBed, async, inject } from '@angular/core/testing';
+/* import { ComponentFixture, TestBed, async, inject, tick, fakeAsync } from '@angular/core/testing';
  import { By } from '@angular/platform-browser';
  import { DebugElement } from '@angular/core'
  import { LoginComponent } from './login.component'
- import { Router, ActivatedRoute } from '@angular/router';
+ import { Router, ActivatedRoute, RouterModule } from '@angular/router';
  import { RouterTestingModule } from '@angular/router/testing';
  import { FormsModule, ReactiveFormsModule } from '@angular/forms'
-
+ import { Http, HttpModule } from '@angular/http';
  import { LoginService } from './login.service'
  import { RouterLinkStubDirective } from '../../../testing/router-stub'
 
  describe('LoginComponent', () => {
-
+   let spy: jasmine.Spy;
    let component: LoginComponent;
    let fixture: ComponentFixture < LoginComponent > ;
    let de: DebugElement;
    let el: HTMLInputElement;
+   let loginservice: LoginService;
+   const res = 'sucess';
+
 
    beforeEach(async(() => {
-
      class RouterStub {
-       navigateByUrl(url: string) { return url; }
+       navigateByUrl(url: string) { return url };
      }
 
-     class FakeLoginService {
-       res = { success: 'true', token: 'JWT ksahkdbzxckhewkcbashcscdk' };
-       findUser() {
-         return this.res;
-       }
-     }
      TestBed.configureTestingModule({
        imports: [
          FormsModule,
          ReactiveFormsModule,
-         RouterTestingModule
+         RouterModule,
+         HttpModule
+
        ],
        declarations: [
          LoginComponent
        ],
        providers: [
          { provide: Router, useClass: RouterStub },
-         { provide: LoginService, useClass: FakeLoginService },
-         { provide: ActivatedRoute }
+         LoginService,
 
-       ]
+       ],
      }).compileComponents();
    }))
 
@@ -50,20 +47,23 @@
      fixture = TestBed.createComponent(LoginComponent);
      console.log('hello')
      component = fixture.componentInstance;
+     loginservice = fixture.debugElement.injector.get(LoginService);
+
+     spy = spyOn(loginservice, 'findUser')
+       .and.returnValue(res);
+
+     de = fixture.debugElement.query(By.css('.button'));
+     el = de.nativeElement;
    })
 
-   it('Navigate when local user log in',
-     inject([LoginService], (LoginService: LoginService) => {
-       component.checkUser('admin@123', 'admin123');
-       const spy = spyOn(LoginService, 'findUser');
-       de = fixture.debugElement.query(By.css('.button'));
-       el = de.nativeElement;
-       //el.click();
-       //component.login();
-       fixture.detectChanges();
-       const navArgs = spy.calls.first().args[0];
-       expect(navArgs).toBe("/dashboard");
-     }));
 
+
+
+   it('should login sucessfully', fakeAsync(() => {
+     fixture.detectChanges();
+     tick();
+     fixture.detectChanges();
+     expect(el.textContent).toBe(res)
+   }))
  })
 */
