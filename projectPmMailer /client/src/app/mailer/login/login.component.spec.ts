aee2b833602ba03f40e5b31a5a8ee0832165b8a2
GauravGupta131220 <grvgupta12@gmail.com>
2017-10-04 09:14:20 +0530

    sign up service test cases added

--- projectPmMailer /client/src/app/mailer/login/login.component.spec.ts
+++ projectPmMailer /client/src/app/mailer/login/login.component.spec.ts
@@ -1,54 +1,57 @@
 /*import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import {RouterTestingModule} from '@angular/router/testing';
+import { By }              from '@angular/platform-browser';
+
+import { DebugElement }    from '@angular/core';
+
+
+import { Router,RouterModule,ActivatedRoute } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from  '@angular/forms'
 import { LoginComponent } from './login.component';
-import{browser,element,by} from 'protractor';
-
-describe('LoginComponent', ()=>{
-
-
- beforeEach(()=>{
-    browser.get('http://localhost:4200');
-    
- })
-  it('should pass as user', function(){
-    
-  element(by.id('email')).sendKeys('user@gmail.com');
-   element(by.id('pwd')).sendKeys('password');
-    element(by.id('loginbutton')).click();
-    browser.pause();
-
- })
-
-});*/
-
-/*import { ComponentFixture, TestBed, async } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core'
-import { LoginComponent } from './login.component'
+import { LoginService } from './login.service';
 
 describe('LoginComponent', () => {
 
-  let component: LoginComponent;
-  let fixture: ComponentFixture < LoginComponent > ;
+  let comp:    LoginComponent;
+  let fixture: ComponentFixture<LoginComponent>;
+  let de:      DebugElement;
+  let el:      HTMLElement;
 
+  // async beforeEach
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [LoginComponent]
-    }).compileComponents();
-  }))
+      imports: [
+            FormsModule,ReactiveFormsModule,RouterModule
+      ],
+      declarations: [LoginComponent], // declare the test component
+      providers: [{provide: Router},{provide: LoginService},{provide: ActivatedRoute}]
 
+    })
+    .compileComponents();  // compile template and css
+  }));
+
+  // synchronous beforeEach
   beforeEach(() => {
     fixture = TestBed.createComponent(LoginComponent);
-    component = fixture.componentInstance;
+
+    comp = fixture.componentInstance; // BannerComponent test instance
+
+    // query for the title <h1> by CSS element selector
+    de = fixture.debugElement.query(By.css('.stockmarket'));
+    el = de.nativeElement;
+  });
+
+
+  it('should be created',() =>{
+    expect(comp).toBeTruthy();
   })
 
-  it('should hit login by google function'), () =>
-    expect(true).toBe(true);
-  /*const expectRes = {};
+  it('should display Access request', () => {
+   fixture.detectChanges();
+   expect(el.textContent).toContain(comp.config.login.STOCK_MARKET);
+ });
 
-  const actualRes = component.loginByGoogle();
+  
 
-  expect(expectRes).toEqual('actualRes')*/
-/*
-})
+   
+});
 */
