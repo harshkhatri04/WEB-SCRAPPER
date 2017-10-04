60fd9ff624dd4b8cd1b22a132bc6a6e0a4a68aa3
onlyshikhar <shikhar.akg@gmail.com>
2017-10-04 10:44:57 +0530

    dashboard service test cases added

--- projectPmMailer /client/src/app/mailer/dashboard/dashboard.service.spec.ts
+++ projectPmMailer /client/src/app/mailer/dashboard/dashboard.service.spec.ts
@@ -1,9 +1,13 @@
-import { TestBed, async, inject } from '@angular/core/testing';
+import { TestBed, async, inject,fakeAsync, tick } from '@angular/core/testing';
+import {Location} from "@angular/common";
 import { Router } from '@angular/router';
+
+
 import {
   HttpModule,
   Http,
   Response,
+  BaseRequestOptions,
   ResponseOptions,
   XHRBackend
 } from '@angular/http';
@@ -11,7 +15,8 @@ import { MockBackend } from '@angular/http/testing';
 import { DashboardService } from './dashboard.service';
 /*import { DashboardComponent } from './dashboard.component';*/
 describe('DashboardService', () => {
-
+ let router: Router;
+ let location: Location;
 let routerStub;
   beforeEach(() => {
     routerStub = {
@@ -27,59 +32,36 @@ let routerStub;
     });
   });
 
- describe('findUser()', () => {
-
-   /* it('Local login should return login credentials',
-      inject([DashboardService, XHRBackend], (loginService, mockBackend) => {
-        const mockResponse = { email: "abc@gmail.com", pwd: "12345" };
-
-
-
-        mockBackend.connections.subscribe((connection) => {
-          connection.mockRespond(new Response(new ResponseOptions({
-            body: JSON.stringify(mockResponse)
-          })));
-        });
-
-        loginService.findUser().subscribe((user) => {
-          expect(user.email).toEqual('abc@gmail.com');
-          expect(user.pwd).toEqual('12345');
-
-        });
-
+ it('can instantiate service when inject service',
+      inject([DashboardService], (service: DashboardService) => {
+        expect(service instanceof DashboardService).toBe(true);
       }));
 
-    it('can instantiate service when inject service',
-    inject([DashboardService], (service: DashboardService) => {
-      expect(service instanceof DashboardService).toBe(true);
-  }));*/
-
- it('can instantiate service when inject service',
-    inject([DashboardService], (service: DashboardService) => {
-      expect(service instanceof DashboardService).toBe(true);
-  }));
 
 
- it('can instantiate service with "new"', inject([Http], (http: Http) => {
-    expect(http).not.toBeNull('http should be provided');
-    let service = new DashboardService(http);
-    DashboardService  }));
+
+    it('can instantiate service with "new"', inject([Http], (http: Http) => {
+      expect(http).not.toBeNull('http should be provided');
+      let service = new DashboardService(http);
+      expect(service instanceof DashboardService).toBe(true, 'new service should be ok');
+    }));
 
 
- it('can provide the mockBackend as XHRBackend',
-    inject([XHRBackend], (backend: MockBackend) => {
-  }));
+    it('can provide the mockBackend as XHRBackend',
+      inject([XHRBackend], (backend: MockBackend) => {
+        expect(backend).not.toBeNull('backend should be provided');
+      }));
 
-
- it('it should navigate to login after user logs out', () => {
-  let component = TestBed.createComponent(DashboardService).componentInstance;
-  component.signout();
-
-  expect(routerStub.navigate).toHaveBeenCalledWith(['/login']);
-})
-  });
-
-   
-  });
+it('navigate to login', 
+  inject([DashboardService,XHRBackend], (dashboardservice,mockBackend) => {
   
+  mockBackend.connections.subscribe((connection)=>{
+    connection.mockRespond(new Response(new ResponseOptions({status:200})))
+  });
+  dashboardservice.signout().subscribe((sign)=>{
+    expect(sign.status).toBe(200);
+  })
+}));
 
+  });
+ 
