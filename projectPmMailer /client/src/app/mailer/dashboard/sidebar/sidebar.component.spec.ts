f569cdb3c1c1fdbc7d584058c3da6e0f6e2a3f8f
rozy tyagi <tyagirozy4@gmail.com>
2017-10-05 17:46:57 +0530

    add test cases for sidebar component

--- projectPmMailer /client/src/app/mailer/dashboard/sidebar/sidebar.component.spec.ts
+++ projectPmMailer /client/src/app/mailer/dashboard/sidebar/sidebar.component.spec.ts
@@ -7,30 +7,68 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SidebarComponent } from './sidebar.component';
 
 describe('SidebarComponent', () => {
-  let component: SidebarComponent;
-  let fixture: ComponentFixture<SidebarComponent>;
-   let de: DebugElement;
-  let el: HTMLElement;
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule,
-        FormsModule,
-        ReactiveFormsModule
-      ],
-      declarations: [ SidebarComponent ]
-    })
-    .compileComponents();
-  }));
+	let component: SidebarComponent;
+	let fixture: ComponentFixture<SidebarComponent>;
+	 let deChart,deDashboard,deSetting,deMails: DebugElement;
+	let elChart,elDashboard,elSetting,elMails: HTMLElement;
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(SidebarComponent);
-    component = fixture.componentInstance;
-    de = fixture.debugElement.query(By.css('.sidebar'));
-    el = de.nativeElement;
-  });
+	//async beforeEach
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			imports: [
+				RouterTestingModule,
+				FormsModule,
+				ReactiveFormsModule
+			],
+			declarations: [ SidebarComponent ]
+		})
+		.compileComponents();
+	}));
+
+//sync beforeEach
+	beforeEach(() => {
+		fixture = TestBed.createComponent(SidebarComponent);
+		component = fixture.componentInstance;
+
+		deChart = fixture.debugElement.query(By.css('.charts'));
+		elChart = deChart.nativeElement;
+
+		deDashboard = fixture.debugElement.query(By.css('.dashboard'));
+		elDashboard = deDashboard.nativeElement;
+
+		deSetting = fixture.debugElement.query(By.css('.settings'));
+		elSetting = deSetting.nativeElement;
+
+		deMails = fixture.debugElement.query(By.css('.mails'));
+		elMails = deMails.nativeElement;
+	});
 it('should create Sidebar Component', () => {
-    const sidebar = fixture.debugElement.componentInstance;
-    expect(sidebar).toBeTruthy();
-  })
+		const sidebar = fixture.debugElement.componentInstance;
+		expect(sidebar).toBeTruthy();
+	})
+
+	 //test case for checking interpolation of DASHBOARD
+	it('should display original category value through interpolation of DASHBOARD', () => {
+		fixture.detectChanges();
+		expect(elDashboard.textContent).toContain(component.config.sidebar.DASHBOARD);
+	});
+
+//test case for checking interpolation of SETTING
+	it('should display original  value through interpolation of SETTING', () => {
+		fixture.detectChanges();
+		expect(elSetting.textContent).toContain(component.config.sidebar.SETTINGS);
+	});
+
+//test case for checking interpolation of MAILS
+	it('should display original  value through interpolation of MAILS', () => {
+		fixture.detectChanges();
+		expect(elMails.textContent).toContain(component.config.sidebar.MAILS);
+	});
+
+//test case for checking interpolation of CHARTS
+	it('should display original  value through interpolation of CHARTS', () => {
+		fixture.detectChanges();
+		expect(elChart.textContent).toContain(component.config.sidebar.CHARTS);
+	});
+
 });
