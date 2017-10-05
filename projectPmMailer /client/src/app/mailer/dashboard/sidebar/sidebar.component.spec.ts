2f3ecdf888d2f71ab586abf1c5f915bea72378c3
rozy tyagi <tyagirozy4@gmail.com>
2017-10-05 17:15:02 +0530

    add test case in sidebar component

--- projectPmMailer /client/src/app/mailer/dashboard/sidebar/sidebar.component.spec.ts
+++ projectPmMailer /client/src/app/mailer/dashboard/sidebar/sidebar.component.spec.ts
@@ -1,13 +1,23 @@
-/*import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router, RouterLinkWithHref } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing'
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SidebarComponent } from './sidebar.component';
 
 describe('SidebarComponent', () => {
   let component: SidebarComponent;
   let fixture: ComponentFixture<SidebarComponent>;
-
+   let de: DebugElement;
+  let el: HTMLElement;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        FormsModule,
+        ReactiveFormsModule
+      ],
       declarations: [ SidebarComponent ]
     })
     .compileComponents();
@@ -16,11 +26,11 @@ describe('SidebarComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(SidebarComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    de = fixture.debugElement.query(By.css('.sidebar'));
+    el = de.nativeElement;
   });
-
-  it('should be created', () => {
-    expect(component).toBeTruthy();
-  });
+it('should create Sidebar Component', () => {
+    const sidebar = fixture.debugElement.componentInstance;
+    expect(sidebar).toBeTruthy();
+  })
 });
-*/
