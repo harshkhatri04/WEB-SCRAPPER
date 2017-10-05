da6ff65267bd6ca5a846aec45eada10b5196fa65
pulkit.5.kakkar <pulkit.5.kakkar@niit-tech.com>
2017-10-05 15:27:33 +0530

    commenting of component Testing

--- projectPmMailer /client/src/app/mailer/dashboard/navbar/navbar.component.spec.ts
+++ projectPmMailer /client/src/app/mailer/dashboard/navbar/navbar.component.spec.ts
@@ -31,12 +31,16 @@ describe('testing navbar component', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(NavbarComponent);
     comp = fixture.componentInstance; //Nav Component instance
+    //For interpolation of DASHBOARD
     deDashboard = fixture.debugElement.query(By.css('.dashboard'));
     elDashboard = deDashboard.nativeElement;
+    //For interpolation of CHARTS
     deCharts = fixture.debugElement.query(By.css('.chart'));
     elCharts = deCharts.nativeElement;
+    //For interpolation of SETTINGS
     deSetting = fixture.debugElement.query(By.css('.settings'));
     elSetting = deSetting.nativeElement;
+    //For interpolation of MAILER
     deMailer = fixture.debugElement.query(By.css('.mailer'));
     elMailer = deMailer.nativeElement;
   })
@@ -47,21 +51,25 @@ describe('testing navbar component', () => {
     expect(login).toBeTruthy();
   });
 
+   //test case for checking interpolation of DASHBOARD
   it('should display original category value through interpolation of DASHBOARD', () => {
     fixture.detectChanges();
     expect(elDashboard.textContent).toContain(comp.config.sidebar.DASHBOARD);
   });
 
+  //test case for checking interpolation of CHARTS
   it('should display original category value through interpolation of CHARTS', () => {
     fixture.detectChanges();
     expect(elCharts.textContent).toContain(comp.config.sidebar.CHARTS);
   });
 
+  //test case for checking interpolation of SETTINGS
   it('should display original category value through interpolation of SETTINGS', () => {
     fixture.detectChanges();
     expect(elSetting.textContent).toContain(comp.config.sidebar.SETTINGS);
   });
 
+  //test case for checking interpolation of MAILER
   it('should display original category value through interpolation of MAILER', () => {
     fixture.detectChanges();
     expect(elMailer.textContent).toContain(comp.config.dashboard.PERSONALISED_MAILER);
