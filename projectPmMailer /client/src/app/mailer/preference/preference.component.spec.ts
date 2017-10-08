a584bd387e67b45afd57101a626746ea90e1b327
rozy tyagi <tyagirozy4@gmail.com>
2017-10-08 18:55:43 +0530

    add test cases in preference component

--- projectPmMailer /client/src/app/mailer/preference/preference.component.spec.ts
+++ projectPmMailer /client/src/app/mailer/preference/preference.component.spec.ts
@@ -1,13 +1,24 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router, RouterLinkWithHref,RouterLink } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement,Directive, Injectable, Input  } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { PreferenceComponent } from './preference.component';
 
 describe('PreferenceComponent', () => {
   let component: PreferenceComponent;
   let fixture: ComponentFixture<PreferenceComponent>;
+   let de: DebugElement;
+  let el: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports:[
+       FormsModule,
+       ReactiveFormsModule
+      ],
       declarations: [ PreferenceComponent ]
     })
     .compileComponents();
@@ -16,10 +27,13 @@ describe('PreferenceComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(PreferenceComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    de = fixture.debugElement.query(By.css('.mails'));
+    el = de.nativeElement;
+
   });
 
   it('should be created', () => {
-    expect(component).toBeTruthy();
+    const preference = fixture.debugElement.componentInstance;
+    expect(preference).toBeTruthy();
   });
 });
