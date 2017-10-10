13d436930ba5c7cb25ad8a3414fb7c8eda9a924c
rozy tyagi <tyagirozy4@gmail.com>
2017-10-10 21:36:11 +0530

    add test cases

--- projectPmMailer /client/src/app/mailer/dashboard/funds/funds.component.spec.ts
+++ projectPmMailer /client/src/app/mailer/dashboard/funds/funds.component.spec.ts
@@ -1,45 +1,43 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Router, RouterLinkWithHref,RouterLink } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DebugElement} from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 import { FundsComponent } from './funds.component';
-
-
-/**
- * [describe description]
- * @param {[type]} 'FundsComponent' [description]
- * @param {[   FundsComponent   ]                })    .compileComponents();  }));  beforeEach(() => {    fixture = TestBed.createComponent(FundsComponent);    component = fixture.componentInstance;    fixture.detectChanges();  });  it('should be created', () => {    expect(component).toBeTruthy();  });}} () => {  let component [description]
- */
+import {NavbarComponent} from '../navbar/navbar.component';
+import {SidebarComponent} from '../sidebar/sidebar.component';
+import {CurrencyService} from '../service/currency.service';
+import {DashboardService} from '../dashboard.service';
+import {TweetService} from '../service/tweet.service';
+import {FundService} from '../service/fund.service';
+ 
 describe('FundsComponent', () => {
   let component: FundsComponent;
   let fixture: ComponentFixture<FundsComponent>;
 
-  /**
-   * configuring module with test environment
-   */
-
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FundsComponent ]
+      imports:[
+      RouterTestingModule,
+        FormsModule,
+        ReactiveFormsModule,
+        HttpModule
+      ],
+      declarations: [ FundsComponent,NavbarComponent,SidebarComponent ],
+      providers: [CurrencyService,DashboardService,TweetService,FundService]
     })
     .compileComponents();
   }));
 
-
-/**
- * [beforeEach description] creating fixtures and components
- * @param {[type]} () => {    fixture = TestBed.createComponent(FundsComponent);    component = fixture.componentInstance;    fixture.detectChanges();  } [description]
- */
   beforeEach(() => {
     fixture = TestBed.createComponent(FundsComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-/**
- *  checking component creation
- * @param {[type]} 'should be created' [description]
- * @param {[type]} ()      => { expect(component).toBeTruthy();  } [description]
- */
-  it('should be created', () => {
-    expect(component).toBeTruthy();
+  it('should be create funds component', () => {
+    const funds = fixture.debugElement.componentInstance;
+    expect(funds).toBeTruthy();
   });
 });
