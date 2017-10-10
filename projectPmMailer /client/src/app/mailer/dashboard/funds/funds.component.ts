d1f8df8e83d6ffa07a44f8660c765be7a0d1a8b9
GauravGupta131220 <grvgupta12@gmail.com>
2017-10-10 11:57:11 +0530

    inline css removed

--- projectPmMailer /client/src/app/mailer/dashboard/funds/funds.component.ts
+++ projectPmMailer /client/src/app/mailer/dashboard/funds/funds.component.ts
@@ -9,12 +9,26 @@ import { FundService } from '../service/fund.service'
   templateUrl: './funds.component.html',
   styleUrls: ['./funds.component.css']
 })
+
+/**
+ * fund component class
+ */
 export class FundsComponent implements OnInit {
   config = config;
   fundnews: String;
 
+
+  /**
+   * [constructor description] initialising services
+   * @param {DashboardService} private DashboardService [description]
+   * @param {Router}           private router           [description]
+   * @param {FundService}      private fund             [description]
+   */
   constructor(private DashboardService: DashboardService, private router: Router, private fund: FundService) {}
 
+  /**
+   * [ngOnInit description] getting funds
+   */ 
   ngOnInit() {
     this.fund.getfund().subscribe((data) => {
 
@@ -25,6 +39,9 @@ export class FundsComponent implements OnInit {
     })
  }
 
+ /**
+  * [logout description] logout method
+  */
   logout() {
     this.DashboardService.signout()
       .subscribe((res) => {
