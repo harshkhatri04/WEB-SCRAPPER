877f75de85997708eeddb6e64b61a6f67e6b522c
shivani <shivanisah96@gmail.com>
2017-10-11 19:49:08 +0530

    added comments in models

--- projectPmMailer /server/express/routes/getData.js
+++ projectPmMailer /server/express/routes/getData.js
@@ -1,17 +1,20 @@
-const User = require('../models/userModel')
-const configure = require('../config/configure');
+/*requiring predefined dependencies*/
 const nodemailer = require('nodemailer');
 const express = require('express');
 const async = require('async');
 const router = express.Router();
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-const logger = require('../services/app.logger');
 const crypto = require('crypto');
 
+/*requiring user defined dependencies*/
+const User = require('../models/userModel')
+const configure = require('../config/configure');
+const logger = require('../services/app.logger');
 
+// route to get user information corresponding to given email
 router.get('/findUser/:email', function(req, res) {
-
+    // finding user corresponding to given email
     User.findOne({ email: req.params.email }, (err, data) => {
         if (err) {
             logger.info("user not found");
