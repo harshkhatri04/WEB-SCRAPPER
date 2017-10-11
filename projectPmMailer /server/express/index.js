9c2d9b274cdd42e3fa3958f952032745d63c4a30
pulkit.5.kakkar <pulkit.5.kakkar@niit-tech.com>
2017-10-11 16:49:56 +0530

    minor bugs fixed

--- projectPmMailer /server/express/index.js
+++ projectPmMailer /server/express/index.js
@@ -1,21 +1,20 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-
-const routes = require('./routes/route');
 const path = require('path');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const session = require('express-session');
+
 const config = require('./config/database')
 const logger = require('./services/app.logger');
 const configure = require('./config/configure')
+const routes = require('./routes/route');
 
 var LocalStrategy = require('passport-local').Strategy;
 var bcrypt = require('bcrypt-nodejs');
 var async = require('async');
 var crypto = require('crypto');
-var flash = require('express-flash');
 
 var cookieParser = require('cookie-parser');
 
@@ -23,8 +22,6 @@ const connect = mongoose.connect(config.database);
 let cors = require('cors');
 
 app.use(cors());
-
-
 
 app.use(session({
     secret: 's3cr3t',
@@ -39,7 +36,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 
 app.use(session({ secret: 'session secret key' }));
-app.use(flash());
 app.use(cookieParser());
 app.use('/', routes);
 
