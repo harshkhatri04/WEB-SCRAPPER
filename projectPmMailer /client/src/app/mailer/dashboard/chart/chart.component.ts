69950e0e001556dd4b3ed8fe35beb8a49fbed609
GauravGupta131220 <grvgupta12@gmail.com>
2017-10-11 15:34:38 +0530

    comments added

--- projectPmMailer /client/src/app/mailer/dashboard/chart/chart.component.ts
+++ projectPmMailer /client/src/app/mailer/dashboard/chart/chart.component.ts
@@ -1,23 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-import {NasdaqService} from '../service/nasdaq.service';
+import { NasdaqService } from '../service/nasdaq.service';
 @Component({
   selector: 'app-chart',
   templateUrl: './chart.component.html',
   styleUrls: ['./chart.component.css']
 })
+
+
+//to generate charts related to nasdaq data
 export class ChartComponent implements OnInit {
 
-  constructor(private nasdaq:NasdaqService) {}
-nasdaqcode:any;
+  constructor(private nasdaq: NasdaqService) {}
+  nasdaqcode: any;
   ngOnInit() {
 
     this.nasdaq.getnasdaqstocks().subscribe((data) => {
 
-     this.nasdaqcode = data;
-     console.log(this.nasdaqcode)
-   }, error => {
-     console.log("Error" + error)
-   })
+      this.nasdaqcode = data;
+      console.log(this.nasdaqcode)
+    }, error => {
+      console.log("Error" + error)
+    })
   }
 
 }
